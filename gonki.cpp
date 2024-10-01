5fd8496aec26c79b3f27b1da0ca8b71d30fad758
gold-sann <molokoedovgrigorij77@gmail.com>
2024-10-01 22:14:53 +0300

    гонки начал

---
+++ gonki.cpp
@@ -0,0 +1,17 @@
+#include <TXLib.h>
+
+int drow_doroga();
+
+int main()
+    {
+    txCreateWindow (800, 800);
+    drow_doroga()
+
+    return 0;
+    }
+
+int drow_doroga()
+    {
+    txLine ();
+    return 0;
+    }
